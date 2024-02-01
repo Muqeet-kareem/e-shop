a0a628ace235e3deae99f79e6c8da245b814e7be
Muqeet-kareem <muqeetkareem04@gmail.com>
2024-02-01 12:19:28 +0100

    updated code

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -36,7 +36,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+    const activationUrl = `https://eshop-store-rk7i.onrender.com/${activationToken}`;
 
     try {
       await sendMail({
